4cfb3e65612fea5b0abe6af19bdd69d8961be431
Rononoa13 <sumitluitel42@gmail.com>
2020-12-05 21:00:58 +0545

    first assignment

---
+++ Assignment 1/Digital Clock/js/app.js
@@ -0,0 +1,11 @@
+window.onload = (event) => {
+    var now = new Date()
+    document.getElementById('clock').innerHTML = now;
+    // console.log(document.getElementById('clock'));
+}
+
+// function startTime() {
+//     var now = new Date()
+
+//     document.getElementById('clock').innerHTML = now;
+// }
---
+++ Assignment 1/js/app.js
@@ -0,0 +1,5 @@
+let favourite_place = ["Biratnagar", "Lalitpur", "Patan", "Kathmandu","Pokhara","Palpa"];
+
+// console.log(favourite_place.length)
+let removed_favourite_place = favourite_place.splice(3,1);
+console.log(favourite_place)
