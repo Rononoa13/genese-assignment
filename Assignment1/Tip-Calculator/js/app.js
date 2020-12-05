57936b6a770346724b32fb1ff269220ec0dda53d
Rononoa13 <sumitluitel42@gmail.com>
2020-12-06 00:04:16 +0545

    fixed calculateTip() function that returns total

--- Assignment1/Tip-Calculator/js/app.js
+++ Assignment1/Tip-Calculator/js/app.js
@@ -3,12 +3,19 @@ function inputBill(){
     let tip = document.getElementById('tip').value
     console.log(bill)
     console.log(tip)
+    // calculateTip(bill, tip)
     console.log(calculateTip(bill, tip));
+    // onDisplayTotal();
+    
 
 }
 
 function calculateTip(bill, tip) {
-    return (tip / 100) + bill;
-    
-
+    let total =  (tip / 100) + bill;
+    document.getElementById('total').innerHTML = "Total: " + total;
+    return total;
 }
+
+// function onDisplayTotal() {
+//     document.getElementById('total').value = calculateTip(bill, tip);
+// }
