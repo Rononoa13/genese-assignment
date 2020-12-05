e188bdac4896664849d75d780b0f065eba2c3cd3
Rononoa13 <sumitluitel42@gmail.com>
2020-12-05 22:20:18 +0545

    Added code to calculate circumference based on the radius

--- Assignment1/js/app.js
+++ Assignment1/js/app.js
@@ -3,3 +3,16 @@ let favourite_place = ["Biratnagar", "Lalitpur", "Patan", "Kathmandu","Pokhara",
 // console.log(favourite_place.length)
 let removed_favourite_place = favourite_place.splice(3,1);
 console.log(favourite_place)
+
+/*
+Create a function called calcCircumfrence:
+
+Pass the radius to the function.
+Calculate the circumference based on the radius, and output "The circumference is NN"
+*/
+
+function calcCircumfrence(radius) {
+    return 2 * Math.PI * radius;
+}
+
+console.log(calcCircumfrence(5))
